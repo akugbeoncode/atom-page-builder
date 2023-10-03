bec4c738167fc2d037d8e7069a71433beb45b3a3
Ode Akugbe <akugbeode@gmail.com>
2023-10-03 10:37:32 +0100

    bug fix: modified login and register page views to be scrollable

--- src/components/containers/projects/ProjectsGrids.jsx
+++ src/components/containers/projects/ProjectsGrids.jsx
@@ -3,7 +3,7 @@ import ProjectGridItem from './ProjectGridItem'
 
 const ProjectsGrids = ({ projects, user, deleteProject, updateProject }) => {
   return (
-    <div className="row g-4 py-5 row-cols-1 row-cols-lg-3">
+    <div className="row g-4 py-3 row-cols-1 row-cols-lg-3">
         {
             projects?.map(project=>(
                 <ProjectGridItem key={project.reference} project={project} user={user} deleteProject={deleteProject} updateProject={updateProject} />
